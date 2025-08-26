5a3814e9d94be1fd0d3d76d712e291da4056184a
krishan-BICT <itt2023074@tec.rjt.ac.lk>
2025-08-26 22:57:05 +0530

    uploaded to repo

---
+++ script.js
@@ -0,0 +1,7 @@
+const manu = document.querySelector("#menu");
+const nav =document.querySelector("#.links");
+
+menu.onclick = () => {
+    menu.classlist.toggle('bx-x');
+    nav.classList.toggle('active');
+}
